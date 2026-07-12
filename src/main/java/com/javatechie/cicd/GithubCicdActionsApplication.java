ff32633dfe8435548f85f4ac379796afeb46f745
abhnvkanth <abhinavkanth234@gmail.com>
2026-07-12 22:40:09 +0545

    initial commit

---
+++ src/main/java/com/javatechie/cicd/GithubCicdActionsApplication.java
@@ -0,0 +1,20 @@
+package com.javatechie.cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class GithubCicdActionsApplication {
+
+    @GetMapping("/welcome")
+    public String welcome(){
+        return "welcome to java";
+    }
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubCicdActionsApplication.class, args);
+	}
+
+}
